cfa69944ea9ad9b769dc53a569d825f04bc340bb
sayandas24 <yosayan007@gmail.com>
2026-04-18 23:25:30 +0530

    update the api key place

--- historystore-genai/genai-history.js
+++ historystore-genai/genai-history.js
@@ -1,23 +1,23 @@
 import { GoogleGenAI } from "@google/genai";
 import readLineSync from 'readline-sync'
 
-const ai = new GoogleGenAI({ apiKey: "AIzaSyCvLCabxJ1dwPpMI98grRnPPL5welEoZO8" });
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 const chat = ai.chats.create({
-    model: "gemini-3-flash-preview",
-    history: []
+  model: "gemini-3-flash-preview",
+  history: []
 });
 
 async function main() {
 
-    const userInput = readLineSync.question("Ask me anything ---> ")
+  const userInput = readLineSync.question("Ask me anything ---> ")
 
-    const response = await chat.sendMessage({
-        message: userInput
-    })
-    console.log(response.text)
+  const response = await chat.sendMessage({
+    message: userInput
+  })
+  console.log(response.text)
 
-    main()
+  main()
 }
 
 main()
